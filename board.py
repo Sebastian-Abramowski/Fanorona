29ce7c9c2e54709b65afa73b18f7a5dd609e4313
Sebastian <sebastian.abramowski@onet.pl>
2023-03-17 10:59:35 +0100

    Resolved the first problem - the drawing method of the board was doing too many different things - now its split into a few different methods as it should be

--- board.py
+++ board.py
@@ -26,12 +26,27 @@ class Board:
     -------
     draw_board(window)
         draws the square boxes depending on the size of passed window
-        (pygame.display) and lines of the board
+        (pygame.display) and lines of the board after calculating
+        its sizes and locations
     row_col_from_center_of_square(passed_center)
         takes as an argument a tuple with two numbers which represents
         the center of the square box and returns its number of column and row
 
         if there is no such square with passed_center, returns None
+    _calculate_size_of_square(width, height)
+        retruns size of the square and sets is to self._square_size
+    draw_vertical_lines(window)
+        draws vertical lines on the window (pygame.display)
+    update_rectangles_and_its_centers(width, height)
+        updates self.rectangles_boxes and self.centers_of_squares,
+        should be called before drawing the board
+    draw_horizontal_lines(window)
+        draws horizontal lines on the window (pygame.display)
+    draw_rectangles(window)
+        draws rectangles on the window (pygame.display), it
+        depends on self.rectangles_boxes attribute
+    draw(window)
+        draws lines and squares on the window (pygame.display)
     """
     def __init__(self):
         self.rows = ROWS
@@ -41,81 +56,15 @@ class Board:
         self._square_size = None
 
     def draw_board(self, window):
-        """Draws the board on the passed window (pygame.display)"""
+        """Calculates measurments of the board and draws the board
+           on the passed window (pygame.display)"""
         # takes the size of the window
         width = window.get_width()
         height = window.get_height()
+        self._calculate_size_of_square(width, height)
+        self.update_rectangles_and_its_centers(width, height)
 
-        # calculating some appropriate size of square
-        square_size_depending_on_height = int(
-            (height-(PADDING*2)) // (((ROWS-1)*2)+1))
-        square_size_depending_on_width = int(
-            (width-(PADDING*2)) // (((COLS-1)*2)+1))
-        square_size = min(
-            square_size_depending_on_height, square_size_depending_on_width)
-        self._square_size = square_size
-
-        middle_points_of_squares = []
-        rectangles_boxes = []
-
-        # centering the board depending on the free space
-        # at the top and at the bottom
-        height_of_board = ((ROWS * square_size) + ((ROWS-1)*square_size))
-        y = (height - height_of_board)//2
-        for _ in range(ROWS):
-            # centering the board depending on the free space
-            # on the left and right
-            width_of_board = ((COLS * square_size) + ((COLS-1) * square_size))
-            x = (width - width_of_board)//2
-            middle_points_in_row = []
-            rectangles_row = []
-            for _ in range(COLS):
-                rectangle = pygame.Rect(x, y, square_size, square_size)
-                rectangles_row.append(rectangle)
-                pygame.draw.rect(window, RED, rectangle)
-                center = rectangle.center
-                middle_points_in_row.append(center)
-                x += 2 * square_size
-            y += 2 * square_size
-            # draw horizontal line
-            # middle_point_of_left is the center of left square in a row
-            # middle_point_of_right is the center of right square in a row
-            middle_point_of_left = middle_points_in_row[0]
-            middle_point_of_right = middle_points_in_row[-1]
-            pygame.draw.line(
-                window, WHITE, middle_point_of_left,
-                middle_point_of_right, LINE_WIDTH)
-
-            middle_points_of_squares.append(middle_points_in_row)
-            rectangles_boxes.append(rectangles_row)
-
-        # assigning lists to attributes of the class
-        self.centers_of_squares = middle_points_of_squares
-        self.rectangles_boxes = rectangles_boxes
-
-        # draw vertical lines
-        for i in range(COLS):
-            middle_point_of_upper = middle_points_of_squares[0][i]
-            middle_point_of_lower = middle_points_of_squares[-1][i]
-            pygame.draw.line(
-                window, WHITE, middle_point_of_upper,
-                middle_point_of_lower, LINE_WIDTH)
-
-        # draw '\' lines
-        for i in range(0, ROWS-2, 2):  # it doesnt take the last row
-            for j in range(0, COLS-2, 2):
-                point_one = middle_points_of_squares[i][j]
-                point_two = middle_points_of_squares[i+2][j+2]
-                pygame.draw.line(
-                    window, WHITE, point_one, point_two, LINE_WIDTH)
-
-        # draw '/' lines
-        for i in range(0, ROWS-2, 2):
-            for j in range(2, COLS, 2):
-                point_one = middle_points_of_squares[i][j]
-                point_two = middle_points_of_squares[i+2][j-2]
-                pygame.draw.line(
-                    window, WHITE, point_one, point_two, LINE_WIDTH)
+        self.draw(window)
 
     def row_col_from_center_of_square(self, passed_center):
         """Takes center of square, returns its number of row, column or None"""
@@ -124,3 +73,97 @@ class Board:
                 if center == passed_center:
                     return i+1, j+1
         return None
+
+    def _calculate_size_of_square(self, width, height):
+        """Calculating some appropriate size of square
+           depending on the width and height of the window"""
+        square_size_depending_on_height = int(
+            (height-(PADDING*2)) // (((ROWS-1)*2)+1))
+        square_size_depending_on_width = int(
+            (width-(PADDING*2)) // (((COLS-1)*2)+1))
+        square_size = min(
+            square_size_depending_on_height, square_size_depending_on_width)
+        self._square_size = square_size
+        return square_size
+
+    def draw_vertical_lines(self, window):
+        """Draws lines, works depending on self.centers_of_squares attribute"""
+        # draw vertical lines
+        for i in range(COLS):
+            middle_point_of_upper = self.centers_of_squares[0][i]
+            middle_point_of_lower = self.centers_of_squares[-1][i]
+            pygame.draw.line(
+                window, WHITE, middle_point_of_upper,
+                middle_point_of_lower, LINE_WIDTH)
+
+        # draw '\' lines
+        for i in range(0, ROWS-2, 2):  # it doesnt take the last row
+            for j in range(0, COLS-2, 2):
+                point_one = self.centers_of_squares[i][j]
+                point_two = self.centers_of_squares[i+2][j+2]
+                pygame.draw.line(
+                    window, WHITE, point_one, point_two, LINE_WIDTH)
+
+        # draw '/' lines
+        for i in range(0, ROWS-2, 2):
+            for j in range(2, COLS, 2):
+                point_one = self.centers_of_squares[i][j]
+                point_two = self.centers_of_squares[i+2][j-2]
+                pygame.draw.line(
+                    window, WHITE, point_one, point_two, LINE_WIDTH)
+
+    def update_rectangles_and_its_centers(self, width, height):
+        """Updates the attributes - self.centers_of_squares
+           and self.rectangles_boxes"""
+        middle_points_of_squares = []
+        rectangles_boxes = []
+
+        # centering the board depending on the free space
+        # at the top and at the bottom
+        height_of_board = ((
+            ROWS * self._square_size) + ((ROWS-1)*self._square_size))
+        y = (height - height_of_board)//2
+        for _ in range(ROWS):
+            # centering the board depending on the free space
+            # on the left and right
+            width_of_board = ((
+                COLS * self._square_size) + ((COLS-1) * self._square_size))
+            x = (width - width_of_board)//2
+            middle_points_in_row = []
+            rectangles_row = []
+            for _ in range(COLS):
+                rectangle = pygame.Rect(
+                    x, y, self._square_size, self._square_size)
+                rectangles_row.append(rectangle)
+                center = rectangle.center
+                middle_points_in_row.append(center)
+                x += 2 * self._square_size
+            y += 2 * self._square_size
+
+            middle_points_of_squares.append(middle_points_in_row)
+            rectangles_boxes.append(rectangles_row)
+
+        # assigning lists to attributes of the class
+        self.centers_of_squares = middle_points_of_squares
+        self.rectangles_boxes = rectangles_boxes
+
+    def draw_horizontal_lines(self, window):
+        """Draws horizontal line"""
+        for row_of_centers in self.centers_of_squares:
+            middle_point_of_left = row_of_centers[0]
+            middle_point_of_right = row_of_centers[-1]
+            pygame.draw.line(
+                window, WHITE, middle_point_of_left,
+                middle_point_of_right, LINE_WIDTH)
+
+    def draw_rectangles(self, window):
+        """Draws rectangles that are in self.rectangles_boxes"""
+        for row_of_rects in self.rectangles_boxes:
+            for rect in row_of_rects:
+                pygame.draw.rect(window, RED, rect)
+
+    def draw(self, window):
+        """Draws lines and squares"""
+        self.draw_rectangles(window)
+        self.draw_vertical_lines(window)
+        self.draw_horizontal_lines(window)
